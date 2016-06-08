4d8924c846874eb353d73caccff5ea6b431324da
Tanguy <trouffignac@enst.fr>
2016-06-08 15:11:10 +0200

    Repartition opur UM

--- Master Shavadoop/src/Main.java
+++ Master Shavadoop/src/Main.java
@@ -6,32 +6,59 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Main {
 
         @SuppressWarnings("CallToPrintStackTrace")
 	public static void main(String[] args) {		
 		try {
-			File file = new File("../" + args[0]);
+                        File file = new File("machines");
+                        File output = new File("actives");
 			BufferedReader fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String line;
-			/*while ((name = fbr.readLine()) != null){
+                        PrintWriter fbw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output))));
+                        String name, s;
+			while ((name = fbr.readLine()) != null){
 				String[] cmdline = {"sh", "-c", "ssh trouffignac@" + name + " echo \"OK\""};
 				Process p = new ProcessBuilder(cmdline).start();				
 				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				while ((s = br.readLine()) != null)
 					if (s.equals("OK"))						
 						fbw.println(name);
-			}*/
-                        int number = 1;
+			}
+                        fbr.close();
+                        fbw.close();
+                        file = new File(args[0]);
+			fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			String line;
+                        int number = 0;
                         while ((line = fbr.readLine()) != null){
-                            File output = new File("../S/S" + number);
-                            PrintWriter fbw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output))));
-                            fbw.write(line);
+                            output = new File("S/S" + number);
+                            fbw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output))));
+                            fbw.println(line);
                             fbw.close();
                             number ++;
                         }
 			fbr.close();
+                        ArrayList<Thread> threads = new ArrayList<>();
+			fbr.close();
+			fbw.close();
+			BufferedReader fbr2 = new BufferedReader(new InputStreamReader(new FileInputStream(new File("actives"))));
+			for(int i = 0 ; i < number ; i ++){
+                            name = fbr2.readLine();
+                            if(name == null){
+                                fbr2 = new BufferedReader(new InputStreamReader(new FileInputStream(output)));
+                                name = fbr2.readLine();
+                            }
+                            MyThread thread = new MyThread(name, i);
+                            threads.add(thread);
+                            thread.start();
+			}
+			fbr.close();
+			for (Thread t : threads){
+				t.join();
+			}
+                        System.out.println("Done");
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
--- Slave Shavadoop/src/Main.java
+++ Slave Shavadoop/src/Main.java
@@ -12,9 +12,9 @@ public class Main {
         @SuppressWarnings("CallToPrintStackTrace")
 	public static void main(String[] args) {		
 		try {
-			File file = new File("../S/S" + args[0]);
+			File file = new File("/cal/homes/trouffignac/SLR207/S/S" + args[0]);
 			BufferedReader fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-                        File output = new File("../Um/Um" + args[0]);
+                        File output = new File("/cal/homes/trouffignac/SLR207/UM/UM" + args[0]);
 			PrintWriter fbw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output))));
 			String line = fbr.readLine();
                         String str[] = line.split(" ");
